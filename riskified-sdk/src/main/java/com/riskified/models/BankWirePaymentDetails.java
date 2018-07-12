7934047e9cc651a3aae30e8528cdb219f2ba05cc
Vinny Martinez <vinny.martinez@riskified.com>
2018-07-12 11:11:11 -0400

    add BankWirePaymentDetails model

---
+++ riskified-sdk/src/main/java/com/riskified/models/BankWirePaymentDetails.java
@@ -0,0 +1,36 @@
+package com.riskified.models;
+
+import com.riskified.validations.*;
+
+public class BankWirePaymentDetails implements IPaymentDetails {
+    private String accountNumber;
+    private String routingNumber;
+
+    public BankWirePaymentDetails(String accountNumber, String routingNumber) {
+        this.accountNumber = accountNumber;
+        this.routingNumber = routingNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public void validate(Validation validationType) throws FieldBadFormatException {
+        if (validationType == Validation.ALL) {
+            Validate.notNullOrEmpty(this, this.accountNumber, "Bank Account Number");
+            Validate.notNullOrEmpty(this, this.routingNumber, "Bank Routing Number");
+        }
+    }
+}
