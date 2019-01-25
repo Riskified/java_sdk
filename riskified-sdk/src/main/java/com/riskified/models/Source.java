8b9f9b4ed66ecb37ee3569791ee08a4207c8eb05
Rich Spitkovsky <richard.spitkovsky@riskified.com>
2019-01-25 10:43:38 -0500

    added source enums

--- riskified-sdk/src/main/java/com/riskified/models/Source.java
+++ riskified-sdk/src/main/java/com/riskified/models/Source.java
@@ -10,6 +10,20 @@ public enum Source {
     mobileWeb,
     @SerializedName("mobile_app")
     mobileApp,
+    @SerializedName("web")
+    web,
+    @SerializedName("chat")
+    chat,
+    @SerializedName("third_party")
+    thirdParty,
+    @SerializedName("phone")
+    phone,
+    @SerializedName("in_store")
+    inStore,
+    @SerializedName("shopify_draft_order")
+    shopifyDraftOrder,
+    @SerializedName("unknown")
+    unknown,
     @SerializedName("other")
     other
 
