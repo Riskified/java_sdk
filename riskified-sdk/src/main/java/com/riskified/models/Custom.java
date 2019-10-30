4a94094b99b42d7708b1b15a10f391ae3ce03cff
arungopalRF <36234947+arungopalRF@users.noreply.github.com>
2019-10-30 18:17:57 -0400

    Revert "Add support for custom fields"

---
+++ riskified-sdk/src/main/java/com/riskified/models/Custom.java
@@ -0,0 +1,20 @@
+package com.riskified.models;
+
+public class Custom {
+    private String app_dom_id;
+    
+    public Custom() {
+    }
+
+    public Custom(String app_dom_id) {
+        this.app_dom_id = app_dom_id;
+    }
+
+    public String getAppDomId() {
+        return app_dom_id;
+    }
+
+    public void setAppDomId(String app_dom_id) {
+        this.app_dom_id = app_dom_id;
+    }
+}
