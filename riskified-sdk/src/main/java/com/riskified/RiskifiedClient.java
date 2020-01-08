899c000742321c6a3fee023eae6679d19cf3583f
Rich Spitkovsky <richard.spitkovsky@riskified.com>
2020-01-08 14:06:09 -0500

    bump version number

--- riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
+++ riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
@@ -951,7 +951,7 @@ public class RiskifiedClient {
         HttpPost postRequest = new HttpPost(url);
         postRequest.setHeader(HttpHeaders.ACCEPT, "application/vnd.riskified.com; version=2");
         postRequest.setHeader("X-RISKIFIED-SHOP-DOMAIN", shopUrl);
-        postRequest.setHeader("User-Agent","riskified_java_sdk/1.2.7"); // TODO: take the version automatically
+        postRequest.setHeader("User-Agent","riskified_java_sdk/1.2.8"); // TODO: take the version automatically
 
         return postRequest;
     }
