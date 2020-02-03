0c97640964cef6fcb2cabbe63e19786a36d42e4f
Rich Spitkovsky <richard.spitkovsky@riskified.com>
2020-02-03 15:41:19 -0500

    remove unnecessary import

--- riskified-sample/src/main/java/com/riskified/samples/orderClient/SimpleClient.java
+++ riskified-sample/src/main/java/com/riskified/samples/orderClient/SimpleClient.java
@@ -2,7 +2,6 @@ package com.riskified.samples.orderClient;
 import java.io.IOException;
 import java.util.*;
 
-import com.riskified.Environment;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpResponseException;
 
