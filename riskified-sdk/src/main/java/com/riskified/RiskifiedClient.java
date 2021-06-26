2a59e1487c685af0a151ad781979f6be678ba9f3
mooseriskified <44244405+mooseriskified@users.noreply.github.com>
2021-06-25 21:05:35 -0400

    update version #

--- riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
+++ riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
@@ -988,7 +988,7 @@ public class RiskifiedClient {
         HttpPost postRequest = new HttpPost(url);
         postRequest.setHeader(HttpHeaders.ACCEPT, "application/vnd.riskified.com; version=2");
         postRequest.setHeader("X-RISKIFIED-SHOP-DOMAIN", shopUrl);
-        postRequest.setHeader("User-Agent","riskified_java_sdk/2.0.0"); // TODO: take the version automatically
+        postRequest.setHeader("User-Agent","riskified_java_sdk/2.0.1"); // TODO: take the version automatically
         postRequest.setHeader("Version","V2");
         return postRequest;
     }
