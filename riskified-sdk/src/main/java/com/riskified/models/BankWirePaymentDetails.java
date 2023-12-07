571cc0d65141259585527b6ef5503945d5d892b1
Bekim Sinanovic <86640828+beksina@users.noreply.github.com>
2023-12-07 13:15:12 -0500

    add token to bank payment

--- riskified-sdk/src/main/java/com/riskified/models/BankWirePaymentDetails.java
+++ riskified-sdk/src/main/java/com/riskified/models/BankWirePaymentDetails.java
@@ -5,10 +5,17 @@ import com.riskified.validations.*;
 public class BankWirePaymentDetails implements IPaymentDetails {
     private String accountNumber;
     private String routingNumber;
+    private String token;
 
     public BankWirePaymentDetails(String accountNumber, String routingNumber) {
         this.accountNumber = accountNumber;
         this.routingNumber = routingNumber;
+    }
+
+    public BankWirePaymentDetails(String accountNumber, String routingNumber, String token) {
+        this.accountNumber = accountNumber;
+        this.routingNumber = routingNumber;
+        this.token = token;
     }
 
     public String getAccountNumber() {
@@ -27,10 +34,19 @@ public class BankWirePaymentDetails implements IPaymentDetails {
         this.routingNumber = routingNumber;
     }
 
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
     public void validate(Validation validationType) throws FieldBadFormatException {
         if (validationType == Validation.ALL) {
             Validate.notNullOrEmpty(this, this.accountNumber, "Bank Account Number");
             Validate.notNullOrEmpty(this, this.routingNumber, "Bank Routing Number");
+            Validate.notNullOrEmpty(this, this.token, "Token");
         }
     }
 }
