a09e79200c0f84c4999aa732caf8578bfcb0335b
Gering112 <45523369+Gering112@users.noreply.github.com>
2024-02-21 17:21:00 -0500

    Implemented recover eligibility for new verification flow

---
+++ riskified-sdk/src/main/java/com/riskified/models/VerificationMethodTypeEnum.java
@@ -0,0 +1,12 @@
+package com.riskified.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum VerificationMethodTypeEnum {
+        @SerializedName("email")
+        email,
+        @SerializedName("cs")
+        cs;
+
+}
+
