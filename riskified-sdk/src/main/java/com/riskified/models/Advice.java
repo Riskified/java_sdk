145d9ea2c0ebe925b6417066209bec66a03785e5
Gering112 <45523369+Gering112@users.noreply.github.com>
2025-04-22 11:23:37 -0400

    Updated Advice class

---
+++ riskified-sdk/src/main/java/com/riskified/models/Advice.java
@@ -0,0 +1,33 @@
+package com.riskified.models;
+import java.util.List;
+
+public class Advice {
+    private boolean inRegulatoryScope;
+    private boolean safeOrder;
+    private List<Recommendation> recommendations;
+
+
+    public boolean getRegulatoryScope() {
+        return inRegulatoryScope;
+    }
+
+    public void setRegulatoryScope(boolean inRegulatoryScope) {
+        this.inRegulatoryScope = inRegulatoryScope;
+    }
+
+    public List<Recommendation> getRecommendation() {
+        return recommendations;
+    }
+
+    public void setRecommendation(List<Recommendation> recommendations) {
+        this.recommendations = recommendations;
+    }
+
+    public boolean getSafeOrder() {
+        return safeOrder;
+    }
+
+    public void setSafeOrder(boolean safeOrder) {
+        this.safeOrder = safeOrder;
+    }
+}
