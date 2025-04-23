10f22570c507bc5bb166caa3e261dbd6e0fedec4
Gering112 <45523369+Gering112@users.noreply.github.com>
2025-04-23 12:34:30 -0400

    Update Client.java

--- riskified-sample/src/main/java/com/riskified/samples/orderClient/Client.java
+++ riskified-sample/src/main/java/com/riskified/samples/orderClient/Client.java
@@ -58,8 +58,6 @@ public class Client {
             System.out.println("-----------------------------------------");  
 
           */
-         //   checkoutOrder;
-         //   String checkoutOrderString = String.valueof(checkoutOrder);
 
 
 
