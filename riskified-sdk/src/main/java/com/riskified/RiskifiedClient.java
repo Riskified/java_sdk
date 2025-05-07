8f9b65b4329a2252dc829f5bda9a749ffc683ed2
Gering112 <45523369+Gering112@users.noreply.github.com>
2025-05-07 13:18:06 -0400

    Removed recipient field from EventLineItem

--- riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
+++ riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
@@ -1075,7 +1075,7 @@ public class RiskifiedClient {
         postRequest.setHeader(HttpHeaders.ACCEPT, "application/vnd.riskified.com; version=2");
         postRequest.setHeader(HttpHeaders.ACCEPT, "application/json");
         postRequest.setHeader("X-RISKIFIED-SHOP-DOMAIN", shopUrl);
-        postRequest.setHeader("User-Agent","riskified_java_sdk/3.0.0"); // TODO: take the version automatically
+        postRequest.setHeader("User-Agent","riskified_java_sdk/3.1.0"); // TODO: take the version automatically
         postRequest.setHeader("Version",versionHeaderValue);
         return postRequest;
     }
