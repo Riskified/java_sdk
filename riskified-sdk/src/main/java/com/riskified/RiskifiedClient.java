244b4cfa7fe357f7ef90e039e6cdc39ec35ee10f
DavidRibeiroRiskified <david.ribeiro@riskified.com>
2026-02-17 10:47:45 +0000

    chore: update version to 5.0.1 (#202)

--- riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
+++ riskified-sdk/src/main/java/com/riskified/RiskifiedClient.java
@@ -1075,7 +1075,7 @@ public class RiskifiedClient {
         postRequest.setHeader(HttpHeaders.ACCEPT, "application/vnd.riskified.com; version=2");
         postRequest.setHeader(HttpHeaders.ACCEPT, "application/json");
         postRequest.setHeader("X-RISKIFIED-SHOP-DOMAIN", shopUrl);
-        postRequest.setHeader("User-Agent","riskified_java_sdk/5.0.0"); // TODO: take the version automatically
+        postRequest.setHeader("User-Agent","riskified_java_sdk/5.0.1"); // TODO: take the version automatically
         postRequest.setHeader("Version",versionHeaderValue);
         return postRequest;
     }
