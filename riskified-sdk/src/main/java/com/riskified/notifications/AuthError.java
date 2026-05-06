289dfe5606eb03deedde9e2a65e47f6dc98ecc3e
DavidRibeiroRiskified <david.ribeiro@riskified.com>
2026-05-06 10:18:47 +0100

    refactor: change logging in auth error & increase version to 6.0.1 (#210)
    * refactor: update log msg
    
    * chore: Increase version to 6.0.1

--- riskified-sdk/src/main/java/com/riskified/notifications/AuthError.java
+++ riskified-sdk/src/main/java/com/riskified/notifications/AuthError.java
@@ -2,7 +2,7 @@ package com.riskified.notifications;
 
 public class AuthError extends Exception {
 
-    public AuthError(String AuthError, String calcAuth) {
-        super(String.format("Auth Error, got hash: %s , expected: %s", AuthError, calcAuth));
+    public AuthError() {
+        super("Request HMAC signature was either missing or incorrect");
     }
 }
